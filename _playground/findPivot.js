db50ec10ea5a1db329964aa857c31ba10862190e
Andy Eastwood <AndrewEastwood@users.noreply.github.com>
2023-07-10 13:59:38 +0300

    Create findPivot.js

---
+++ _playground/findPivot.js
@@ -0,0 +1,20 @@
+const findPivot = (nums, left, right) => {
+  const midIdx = left + Math.floor((right - left) / 2);
+  // check mid value and its
+  if (left === right) { return left; }
+  if (left > right) { return -1; }
+  if (nums[midIdx] > nums[midIdx + 1]) { return midIdx + 1; }
+  if (nums[midIdx - 1] > nums[midIdx] && nums[midIdx] < nums[midIdx + 1]) { return midIdx; }
+
+  if (nums[midIdx] > nums[right]) {
+    return findPivot(nums, midIdx + 1, right);
+  }
+  return findPivot(nums, left, midIdx - 1);
+}
+
+// success:
+findPivot([1,2,3,4,5,6,7,0], 0, 7)
+findPivot([2,3,4,5,6,7,0,1], 0, 7)
+findPivot([7,0,1,2,3,4,5,6], 0, 7)
+findPivot([0,1,2,3,4,5,6,7], 0, 7)
+findPivot([5,6,7,0,1,2,3,4], 0, 7)
