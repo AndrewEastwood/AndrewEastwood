509017ba3ad236e2b5322f65312bf8cacf613ab9
Andy Eastwood <AndrewEastwood@users.noreply.github.com>
2023-07-14 14:26:02 +0300

    walkerBFS

--- _playground/binaryTree.js
+++ _playground/binaryTree.js
@@ -1,22 +1,58 @@
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+const dummyTree = new TreeNode(0, new TreeNode(1, new TreeNode(3), new TreeNode(4)), new TreeNode(2))
+
+/*
+         0
+       /   \
+      1     2
+     / \
+    3   4
+*/
+
 // DFS Variations
-const walkerInOrder = (node, pathSum = 0, p = []) => {
+const walkerInOrder = (node) => {
   if (!node) { return; }
-  walkerInOrder(node.left, pathSum + node.val, [...p, node.val]);
+  walkerInOrder(node.left);
   console.log('in-order', node.val);
-  walkerInOrder(node.right, pathSum + node.val, [...p, node.val]);
+  walkerInOrder(node.right);
 };
-const walkerPreOrder = (node, pathSum = 0, p = []) => {
+walkerInOrder(dummyTree);
+// 3, 1, 4, 0, 2
+
+const walkerPreOrder = (node) => {
   if (!node) { return; }
   console.log('pre-order', node.val);
-  walkerPreOrder(node.left, pathSum + node.val, [...p, node.val]);
-  walkerPreOrder(node.right, pathSum + node.val, [...p, node.val]);
+  walkerPreOrder(node.left);
+  walkerPreOrder(node.right);
 };
-const walkerPostOrder = (node, pathSum = 0, p = []) => {
+walkerPreOrder(dummyTree);
+// 0, 1, 3, 4, 2
+
+const walkerPostOrder = (node) => {
   if (!node) { return; }
-  walkerPostOrder(node.left, pathSum + node.val, [...p, node.val]);
-  walkerPostOrder(node.right, pathSum + node.val, [...p, node.val]);
+  walkerPostOrder(node.left);
+  walkerPostOrder(node.right);
   console.log('post-order', node.val);
 };
+walkerPostOrder(dummyTree);
+// 3, 4, 1, 2, 0
 
 
 // BFS
+const walkerBFS = (node) => {
+  const q = [];
+  if (node) { q.push(node); }
+  while (q.length) {
+    const item = q.shift();
+    console.log('bfs', item.val);
+    if (item.left) { q.push(item.left); }
+    if (item.right) { q.push(item.right); }
+  }
+}
+walkerBFS(dummyTree);
+// 0, 1, 2, 3, 4
