fa4ec73138faf49077d6a36fcdd4ac464d5265d6
Andy Eastwood <AndrewEastwood@users.noreply.github.com>
2023-07-21 18:35:38 +0300

    preorderTraversal

---
+++ 144. Binary Tree Preorder Traversal.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  const walk = (node, v = []) => {
+    if (!node) { return v; }
+    v.push(node.val);
+    walk(node.left, v);
+    walk(node.right, v);
+    return v;
+  }
+  return walk(root);
+};
